57eda574d44a929578c21f935f611daca62e1068
evgeni83 <evgeni.daniluk@yandex.ru>
2021-03-10 13:12:27 +0300

    Homework for Lesson 3.2

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { msg1, msg2 } from './variables';
+import showAlert from './showAlert';
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+showAlert(msg1);
+showAlert(msg2);
---
+++ src/variables.js
@@ -0,0 +1,2 @@
+export const msg1 = 'Первое сообщение';
+export const msg2 = 'Второе сообщение';
